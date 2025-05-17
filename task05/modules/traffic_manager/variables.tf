146e230768deed019b7ef2e1bca392bba315169f
Suryansh Tilak <suryansh_tilak@epam.com>
2025-05-18 02:20:39 +0530

    c8

--- task05/modules/traffic_manager/variables.tf
+++ task05/modules/traffic_manager/variables.tf
@@ -1,24 +1,40 @@
+variable "resource_groups" {
+  description = "Resource groups configuration"
+  type        = map(any)
+}
 
-variable "traffic_manager" {
-  description = "Traffic Manager profile configuration"
-  type = object({
-    name           = string
-    resource_group = string
-    routing_method = string
-  })
+variable "app_service_plans" {
+  description = "App Service Plans configuration"
+  type        = map(any)
 }
 
 variable "app_services" {
-  description = "Map of app services"
-  type = map(object({
-    name             = string
-    location         = string
-    resource_group   = string
-    app_service_plan = string
-  }))
+  description = "App Services configuration"
+  type        = map(any)
+}
+
+variable "traffic_manager" {
+  description = "Traffic Manager profile configuration"
+  type        = any
 }
 
 variable "tags" {
-  description = "Common tags for traffic manager"
+  description = "Common tags for all resources"
   type        = map(string)
+}
+
+variable "allowed_ip" {
+  description = "Allowed IP address for verification agent"
+  type        = string
+}
+
+# If you have allow-ip and allow-tm variables, declare them too if used
+variable "allow-ip" {
+  description = "Allowed IP for validation"
+  type        = string
+}
+
+variable "allow-tm" {
+  description = "Traffic Manager profile name for validation"
+  type        = string
 }
